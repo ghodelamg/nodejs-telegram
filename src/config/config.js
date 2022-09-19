3bac060989538b885e23dd8cd39ffcd025d0ebcf
Mukesh <mukesh.easternts@gmail.com>
2022-09-19 07:11:06 +0530

    Changed in config

--- src/config/config.js
+++ src/config/config.js
@@ -8,18 +8,20 @@ const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
     PORT: Joi.number().default(3000),
-    MONGODB_URL: Joi.string().required().description('mongodb+srv://cluster0.e4dgflc.mongodb.net/myFirstDatabase/'),
+    MONGODB_URL: Joi.string().required().description('mongodb+srv://cluster0.e4dgflc.mongodb.net/'),
     JWT_SECRET: Joi.string()
       .required()
       .description(
         'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik11a2VzaCBHaG9kZWxhIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.JSYGZPlXPwAPqSBVU7Srpd5St3nJtNzjLHV6suGVbl5Zcz5Ko6h-oqwnyZgm8Up5M-cKRMmQ3QwuuHccgX3t1fqwEDFiKorWk8csUnlPrpy61vqDaa4B2lib70yhREJ6fqg_exo9ZgvmSCjvmqo8KFYf-AucDWdHSl1KXq4eUp-Q9KaY71suZYQEOj6k-N-OLAbVOw5Qlytd6CmCtZeb-zWX1Z-7vgkeyHJo2IX_KMjM-LDBUVwqjJtR-jUjoe2VUZmrYsfxQ1U5G5eksH2t9GMXQ9CztjEFRN3Yz9sRELkADIuXzcTMbjGfd_ubpCuHnQUDXVqOW47IslMvh2pMiA'
       ),
-    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description(3000),
-    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description(365),
+    JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
+    JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
     JWT_RESET_PASSWORD_EXPIRATION_MINUTES: Joi.number()
       .default(10)
       .description('minutes after which reset password token expires'),
-    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number().default(10).description('ghodelamg@gmail.com'),
+    JWT_VERIFY_EMAIL_EXPIRATION_MINUTES: Joi.number()
+      .default(10)
+      .description('minutes after which verify email token expires'),
     SMTP_HOST: Joi.string().description('smtp.ethereal.email'),
     SMTP_PORT: Joi.number().description('587'),
     SMTP_USERNAME: Joi.string().description('robin.barton@ethereal.email'),
